7b19dd8a3dcabc905956a17044d17efd1f409eec
GianlucaPanzani <gianluca.panzani97@gmail.com>
2026-04-15 14:51:54 +0200

    benchmark

--- src/grid/seq_grid_search.sh
+++ src/grid/seq_grid_search.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Fixed values for the benchmark campaign
-N_VALUE=10000000
+N_VALUE=50000000
 P_VALUE=256
 
 # Variable values explored by benchmark.sh
 SEED_VALUES=(13)
-MAX_KEY_VALUES=(262144 1048576)
+MAX_KEY_VALUES=(65536 262144)
 PARTITION_THREAD_VALUES=(1)
 JOIN_THREAD_VALUES=(1)
